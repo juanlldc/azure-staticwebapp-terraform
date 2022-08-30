8af8fd0f88db0326e3f3b3aa680bc77f680b3d7a
Joshua Koehler <joshua.koehler86@gmail.com>
2022-08-29 23:55:20 -0500

    fix name misnomer

--- terraform_infrastructure/main.tf
+++ terraform_infrastructure/main.tf
@@ -28,16 +28,16 @@ resource "azurerm_static_site" "this" {
 }
 
 output "static_web_app_id" {
-  value = azurerm_static_site.az204.id
+  value = azurerm_static_site.this.id
   description = "Id for static site app"
 }
 
 output "default_host_name" {
-  value = azurerm_static_site.az204.default_host_name
+  value = azurerm_static_site.this.default_host_name
   description = "Host name for static site"
 }
 
 output "api_key" {
-  value = azurerm_static_site.az204.api_key
+  value = azurerm_static_site.this.api_key
   description = "API key used by GitHubAction for static web app"
 }
